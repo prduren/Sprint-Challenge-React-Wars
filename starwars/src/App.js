c2d3cf3a0ba3145f9c591265717187b2ee0f5adf
pduren <57735971+kParkz@users.noreply.github.com>
2020-05-15 09:21:05 -0500

    working first API fetch, data displaying

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Character from "./components/Character";
+import useAxios from "axios-hooks";
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [{ data, loading, error }] = useAxios(
+    "https://swapi.py4e.com/api/people/"
+  );
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{JSON.stringify(error, null, 2)}</p>;
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.results.map((char) => (
+        <Character key={char.url} char={char} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,30 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+function Character({ char }) {
+  return (
+    <StyledDiv>
+      <h2>{char.name}</h2>
+      <p>Height: {char.height}</p>
+      <p>Gender: {char.gender}</p>
+      <p>Birth Year: {char.birth_year}</p>
+      <p>Skin Color: {char.skin_color}</p>
+      <p>Hair Color: {char.hair_color}</p>
+    </StyledDiv>
+  );
+}
+
+const StyledDiv = styled.div`
+  border: 2px solid black;
+  width: 20rem;
+  height: 30rem;
+  margin: 2rem;
+  font-size: 16px;
+  color: black;
+  background: #bcc8c1;
+  height: auto;
+  border-radius: 4px;
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+`;
+
+export default Character;
